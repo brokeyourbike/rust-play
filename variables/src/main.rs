2b9763ee76c7f922ffb5aee9d6ed160b408803fb
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-03-13 01:13:33 +0200

    chore: destruct the tup

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,4 +8,8 @@ fn main() {
     let x = x + MAX_POINTS;
     let x = x * 2;
     println!("The value of x is: {}", x);
+
+    let tup = (500, 6.4, 1);
+    let (_, _, z) = tup;
+    println!("The value of z is: {}", z);
 }
