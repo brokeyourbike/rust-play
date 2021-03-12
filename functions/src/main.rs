f752d6d88bfdebb689953a014a3659d4ef7372d0
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-03-13 01:41:21 +0200

    feat: functions

---
+++ functions/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let x = five();
+
+    let y = {
+        let x = plus_one(3);
+        x
+    };
+
+    another_function(y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);
+}
