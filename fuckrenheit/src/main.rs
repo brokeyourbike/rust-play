8b52585b2b3354b74e0dae9b9671559fc7158973
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-03-13 02:03:37 +0200

    feat: fuckrenheit

---
+++ fuckrenheit/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+fn main() {
+    println!("Convert celcius to fuckrenheit!");
+
+    loop {
+        println!("Please input your temperature in celcius:");
+
+        let mut celsius = String::new();
+
+        io::stdin()
+            .read_line(&mut celsius)
+            .expect("We trying to fuckrenheit, do not mess your damn input!");
+
+        let celsius: f64 = match celsius.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("What the heck are you typing!??");
+                continue
+            },
+        };
+
+        let fuckrenheit = celsius * 1.8 + 32.0;
+
+        break println!("Fuckrenheit value: {}", fuckrenheit);
+    }
+}
