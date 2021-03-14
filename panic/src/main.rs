50b99424f52aa59e9be2a1602161f1ba2c96894e
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-03-14 04:12:06 +0200

    feat: panic

---
+++ panic/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    let username = read_username_from_file();
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
