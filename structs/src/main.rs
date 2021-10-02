d6437f40820ed7fefbf347a3b7be2302f27e04a9
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-10-02 15:54:59 +0300

    fix: finish structs chapter

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,40 +1,46 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
 
 fn main() {
-    let mut user1 = build_user(String::from("admin@kek.com"), String::from("admin"));
-
-    println!("{}", user1.email);
-
-    user1.email = String::from("lol@kek.com");
-
-    println!("{}", user1.email);
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        username: String::from("anotherusername567"),
-        ..user1
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let rect4 = Rectangle::square(30);
 
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 
-    println!("{}", black.0);
-}
+    println!("rect1 is {:?}", &rect1);
 
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold rect4? {}", rect1.can_hold(&rect4));
 }
